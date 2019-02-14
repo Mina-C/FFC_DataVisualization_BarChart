4973534ed5791e4c73a29c17df49447797e1427d
meanyMina <42491537+meanyMina@users.noreply.github.com>
2019-02-14 22:19:41 +0100

    Bar using JSON is completed

--- js.js
+++ js.js
@@ -1,39 +1,26 @@
-req = new XMLHttpRequest();
-req.open("GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
-req.send();
-req.onload=function(){
-  json=JSON.parse(req.responseText);
-  var dataset = json['data'];
-  return dataset[0];
-}
-
 d3.select('body').append('h1')
   .text('United States GDP')
   .attr("id", "title");
 
-d3.select('body').append('p').text(dataset[0]);
+d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json").then(function(json){
+  
+  const dataset = json.data; // ["1947-01-01", 243.1]
+  const w = 800;
+  const h = 500;
+  const padding = 60;
+  const rectWid = (w - padding)/dataset.length;
 
-/* 
-const w = 1000;
-const h = 500;
-const padding = 60;
-
-const xScale = d3.scaleLinear()
-  .domain([d3.min(dataset, (d) => d[0]),              d3.max(dataset, (d) => d[0])])
-  .range([padding, x - padding]);
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(dataset, (d) => d[0])])
-  .range([h-padding, padding]);
-const svg = d3.select("body").append('svg')
-  .attr('width', w).attr('height', h);
-
-svg.selectAll("rect")
-.data(dataset)
-.enter().append("rect")
-.attr("x", (d, i) => i * 30)
-.attr("y", (d, i) => h - 3 * d)
-
-*/
-
-
-
+  const yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, (d) => d[1])])
+        .range([h - padding, padding]);
+  
+  const svg = d3.select("body").append('svg')
+        .attr('width', w).attr('height', h);
+  
+  svg.selectAll('rect').data(dataset).enter().append('rect')
+        .attr('x', (d,i) => padding + i*rectWid)
+        .attr('y', (d,i) => yScale(d[1]))
+        .attr('width', rectWid)
+        .attr("height", (d, i) => h - padding - yScale(d[1]));
+  
+})
